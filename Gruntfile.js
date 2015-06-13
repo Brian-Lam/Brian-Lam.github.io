69d579f70fb56a355b1617bb94e8f05e531b0aed
Brian Lam <iBriaan@gmail.com>
2015-06-13 18:53:36 -0400

    JS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,12 +23,22 @@ module.exports = function(grunt) {
       build: {
         src: ["style/*.css.map"]
       }
-}
+    },
+     uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'dest/output.min.js': ['src/input.js']
+        }
+      }
+  }
   });
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-clean');
-
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('default', ['watch']);
 };
